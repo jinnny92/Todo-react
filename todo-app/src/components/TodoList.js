cc13e51f22433376d63ff3ecafab1451fd7372ff
jinnny92 <jinnny22@naver.com>
2022-08-20 14:47:59 +0900

    Todo항목추가

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,15 +1,15 @@
-import React from "react";
-import TodoListItem from "./TodoListItem";
-import './TodoList.scss'
+import React from 'react';
+import TodoListItem from './TodoListItem';
+import './TodoList.scss';
 
-const TodoList = () =>{
-    return(
-        <div className="TodoList">
-            <TodoListItem/>
-            <TodoListItem/>
-            <TodoListItem/>
-        </div>
-    )
-}
+const TodoList = ({ todos }) => {
+  return (
+    <div className="TodoList">
+      {todos.map((todo) => (
+        <TodoListItem todo={todo} key={todo.id} />
+      ))}
+    </div>
+  );
+};
 
 export default TodoList;
